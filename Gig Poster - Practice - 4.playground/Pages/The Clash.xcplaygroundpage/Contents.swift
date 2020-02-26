edb957424d9065194a01cfb197baf93d5e3c585c
WilliamChan <wchan@lcs.on.ca>
2020-02-26 11:53:04 -0500

    finished squares

--- Gig Poster - Practice - 4.playground/Pages/The Clash.xcplaygroundpage/Contents.swift
+++ Gig Poster - Practice - 4.playground/Pages/The Clash.xcplaygroundpage/Contents.swift
@@ -33,6 +33,53 @@ let black = Color(hue: 0, saturation: 100, brightness: 0, alpha: 100)
 
 // Begin your work below...
 
+// draw background
+canvas.fillColor = orange
+canvas.drawRectangle(at: Point(x: 0, y: 0), width: 400, height: 600)
+
+// draw blue squares
+canvas.drawShapesWithBorders = true
+canvas.drawShapesWithFill = false
+canvas.rotate(by: 45)
+canvas.borderColor = blue
+
+
+for y in stride(from: -1, to: 300, by: 50) {
+    for x in stride(from: -1, to: 300, by: 50) {
+        if x == y {
+            canvas.drawRectangle(at: Point(x: 400, y: 150), width: x, height: y, anchoredBy: AnchorPosition.centre, borderWidth: 10)
+            
+        }
+    }
+}
+
+// draw white squares
+canvas.drawShapesWithBorders = true
+canvas.drawShapesWithFill = false
+canvas.borderColor = offWhite
+
+
+for y in stride(from: -1, to: 300, by: 50) {
+    for x in stride(from: -1, to: 300, by: 50) {
+        if x == y {
+            canvas.drawRectangle(at: Point(x: 450, y: 100), width: x, height: y, anchoredBy: AnchorPosition.centre, borderWidth: 10)
+            
+        }
+    }
+}
+
+// draw text
+canvas.drawText(message: "with the brattles", at: Point(x: 17, y: 40), size: 8)
+canvas.drawText(message: "september 12, 13, 14, 1975", at: Point(x: 17, y: 20), size: 8)
+
+canvas.drawText(message: "at cbgb and omfug", at: Point(x: 150, y: 40), size: 8)
+canvas.drawText(message: "315 bowery, new york city", at: Point(x: 150, y: 20), size: 8)
+
+canvas.drawText(message: "also appearing:", at: Point(x: 275, y: 40), size: 8)
+canvas.drawText(message: "from brooklyn, the shirts", at: Point(x: 275, y: 20), size: 8)
+
+canvas.drawText(message: "talking heads", at: Point(x: 20, y: 150), size: 40)
+
 
 /*:
  ## Use Source Control
